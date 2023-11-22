77f68f5e4e899aa1100ecf2dfe4dc65aaa353c5c
Dambu Mirela <cristidambu@gmail.com>
2023-11-22 15:14:20 +0200

    first commit

---
+++ Elm-Project-updated/src/Model/PersonalDetails.elm
@@ -0,0 +1,29 @@
+module Model.PersonalDetails exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, classList, href, id)
+
+
+type alias DetailWithName =
+    { name : String
+    , detail : String
+    }
+
+
+type alias PersonalDetails =
+    { name : String
+    , contacts : List DetailWithName
+    , intro : String
+    , socials : List DetailWithName
+    }
+
+
+view : PersonalDetails -> Html msg
+view details =
+     div [] [
+        h1 [id "name"][text details.name],
+        em [id "intro"][text details.intro],
+        ul [] <| List.map(\x -> li[class "contact-detail"][text x.detail]) details.contacts,
+        ul [] <| List.map (\x -> li[class "social-link"][a[href x.detail][text x.name]]) details.socials
+     ]
+    --Debug.todo "Implement the Model.PersonalDetails.view function"
